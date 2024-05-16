4dd2d631dc14358e7a544fdcfa32fa449fc223f1
danielagtrzp <lgutierrezparra@deloitte.com>
2024-05-16 15:24:00 -0600

    Creating a Regions controller and hardcoding a GetAll endpoint

---
+++ NZWalksAPI/Controllers/RegionsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        //Retrieve all regions hardcoded
+        //GET: https://localhost:44325/api/Regions
+        [HttpGet]
+        public IActionResult GetAllRegions()
+        {
+            List<Region> regions = new List<Region>{
+                new Region()
+                {
+                    Id= Guid.NewGuid(),
+                    Code = "some",
+                    Name = "RegionA",
+                    RegionImgUrl = "https://someImage1"
+                },
+                new Region()
+                {
+                    Id= Guid.NewGuid(),
+                    Code = "some2",
+                    Name = "RegionB",
+                    RegionImgUrl = "https://someImage2"
+                }
+            };
+
+            return Ok(regions);
+        }
+    }
+}
